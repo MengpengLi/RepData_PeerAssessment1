97bcd3cff5a7909e788005fafd69e9229ef0ce57
MengpengLi <pgwd749364@gmail.com>
2016-02-05 10:00:31 +0000

    submit

--- RepResearch.R
+++ RepResearch.R
@@ -24,3 +24,26 @@ steps2 <- cbind(as.Date(row.names(steps2)),steps2)
 colnames(steps2) = c("date","sum")
 plot(steps2$date,steps2$sum,type = "h")
 
+weekDay <- c("Monday","Tuesday","Wednesday","Thursday","Friday")
+weekEnd <- c("Saturday","Sunday")
+week <- weekdays(Filled$date)
+for(i in 1:length(week)){
+      if(length(which(week[i] == weekDay)) != 0){
+           week[i] = "Weekdays" 
+      }else if(which(week[i] == weekEnd)){
+            week[i] = "Weekends"
+      }
+}
+Filled <- cbind(Filled, week)
+par(mfrow = c (2,1))
+
+FilledWeekDays <- Filled[Filled$week == "Weekdays",]
+FilledWeekEnds <- Filled[Filled$week == "Weekends",]
+series2_1 <- data.frame(tapply(FilledWeekDays$steps, FilledWeekDays$interval, mean))
+series2_2 <- data.frame(tapply(FilledWeekEnds$steps, FilledWeekEnds$interval, mean))
+series2_1 <- cbind(as.numeric(row.names(series2_1)),series2_1)
+series2_2 <- cbind(as.numeric(row.names(series2_2)),series2_2)
+colnames(series2_1) <- c("interval","mean")
+colnames(series2_2) <- c("interval","mean")
+plot(series2_1$interval,series2_1$mean, type = "l")
+plot(series2_2$interval,series2_2$mean, type = "l")
